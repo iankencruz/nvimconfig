5b1cda785666094fc8c34a42ab220a2324e00e16
Ian Cruz <iankencruz@gmail.com>
2023-12-15 02:08:10 +1100

    staged init.lua changes

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
 require "config.init"
 require "config.keymaps"
 require "config.lazy"
-require("leap").add_default_mappings()
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,8 +14,17 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup {
   { import = "plugins" },
   { import = "plugins.lsp" },
+  diff = {
+    -- diff command <d> can be one of:
+    -- * browser: opens the github compare view. Note that this is always mapped to <K> as well,
+    --   so you can have a different command for diff <d>
+    -- * git: will run git diff and open a buffer with filetype git
+    -- * terminal_git: will open a pseudo terminal with git diff
+    -- * diffview.nvim: will open Diffview to show the diff
+    cmd = "git",
+  },
   checker = {
-    enabled = true,
+    enabled = false,
     notify = false,
   },
   change_detection = {
