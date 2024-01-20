e38e9e82a47ac6dab2e0440288cd8f31c33f2771
Ian Cruz <iankencruz@gmail.com>
2024-01-21 03:43:59 +1100

    new config setup

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,69 @@
+--
+-- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+-- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+-- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+-- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+-- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+-- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+--
+-- File: config/keymaps.lua
+-- Description: Key mapping configs
+
+
+
+-- general KEYMAPS
+vim.keymap.set("i", "jk", "<ESC>")
+
+-- Clear search with <esc>
+vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+
+-- better indenting
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- lazy
+vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+-- Close all windows and exit from Neovim with <leader> and q
+vim.keymap.set("n", "<leader>q", ":qa!<CR>", {})
+
+-- Fast saving with <leader> and s
+vim.keymap.set("n", "<leader>s", ":w<CR>", {})
+
+-- Move around splits
+vim.keymap.set("n", "<C-h>", "<C-w>h", {})
+vim.keymap.set("n", "<C-j>", "<C-w>j", {})
+vim.keymap.set("n", "<C-k>", "<C-w>k", {})
+vim.keymap.set("n", "<C-l>", "<C-w>l", {})
+
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v")     -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s")     -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=")     -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+-- resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<c-up>", "<cmd>resize +2<cr>", { desc = "increase window height" })
+vim.keymap.set("n", "<c-down>", "<cmd>resize -2<cr>", { desc = "decrease window height" })
+vim.keymap.set("n", "<c-left>", "<cmd>vertical resize +2<cr>", { desc = "increase window width" })
+vim.keymap.set("n", "<c-right>", "<cmd>vertical resize -2<cr>", { desc = "decrease window width" })
+
+-- Reload configuration without restart nvim
+vim.keymap.set("n", "<leader>r", ":so %<CR>", {})
+
+-- Telescope
+-- <leader> is a space now
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+-- NvimTree
+vim.keymap.set("n", "<leader>ee", ":NvimTreeToggle<CR>", {})   -- open/close
+vim.keymap.set("n", "<leader>er", ":NvimTreeRefresh<CR>", {})  -- refresh
+vim.keymap.set("n", "<leader>ef", ":NvimTreeFindFile<CR>", {}) -- search file
+
+-- Terminal
+vim.keymap.set("n", "<leader>tt", ":NeotermToggle<CR>", {})
+-- vim.keymap.set("n", "<leader>tx", ":NeotermExit<CR>", {})
