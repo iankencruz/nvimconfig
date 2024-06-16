04696b8a7397d63c9b610e6e8659dd843b961532
iankendoit <iankencruz@gmail.com>
2024-06-16 13:26:28 +1000

    config refresh - delete

--- init.lua
+++
@@ -1,6 +0,0 @@
-if vim.fn.has('nvim-0.8') == 0 then
-  error('Need Neovim 0.8+ in order to use this config')
-end
-
--- Load main config
-require("config")
--- lua/plugins/goto-preview.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'rmagatti/goto-preview',
-  config = function()
-    require('goto-preview').setup {
-      width = 200,
-      height = 25,
-    }
-  end
-}
