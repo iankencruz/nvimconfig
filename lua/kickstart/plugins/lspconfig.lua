7e62b9c462d1c59a2c7d50ce191c2621a54e402e
iankendoit <iankencruz@gmail.com>
2024-09-05 11:37:52 +1000

    add typescript to servers

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -169,18 +169,15 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
+
+        -- Typescript LSP
+        tsserver = {},
 
         -- Astro LSP
         astro = {},
@@ -209,7 +206,9 @@ return {
           },
         },
 
-        -- emmet_language_server {},
+        emmet_language_server = {
+          filetypes = { 'templ', 'html', 'astro', 'typescript', 'react' },
+        },
 
         -- templ LSP
         templ = {
