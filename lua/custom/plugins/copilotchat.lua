d4dc03ab1574b63a617f48edfa0c217f8e779778
iankendoit <iankencruz@gmail.com>
2025-03-30 01:01:07 +1100

    new confi

--- lua/custom/plugins/copilotchat.lua
+++ lua/custom/plugins/copilotchat.lua
@@ -1,54 +1,62 @@
 return {
   {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    branch = 'canary',
+    'CopilotC-Nvim/CopilotChat.nvim', -- Plugin repository
+    branch = 'main', -- Branch to use
     dependencies = {
-      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
-      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+      { 'zbirenbaum/copilot.lua' }, -- Dependency for Copilot functionality
+      { 'nvim-lua/plenary.nvim' }, -- Dependency for utility functions
     },
-    build = 'make tiktoken', -- Only on MacOS or Linux
+    build = 'make tiktoken', -- Build command, only for MacOS or Linux
     opts = {
       debug = true, -- Enable debugging
+      show_help = true, -- Show help message as virtual lines when waiting for user input
+      show_folds = true, -- Show folds for sections in chat
+      highlight_selection = true, -- Highlight selection
+      highlight_headers = true, -- Highlight headers in chat, disable if using markdown renderers
+      auto_follow_cursor = true, -- Auto-follow cursor in chat
+      auto_insert_mode = true, -- Automatically enter insert mode when opening window and on new prompt
+      insert_at_end = true, -- Move cursor to end of buffer when inserting text
+
       window = {
-        layout = 'float',
-        relative = 'cursor',
-        width = 1,
-        height = 0.4,
-        row = 1,
+        layout = 'float', -- Window layout type
+        relative = 'editor', -- Window position relative to the editor
+        width = 0.97, -- Window width as a fraction of the editor width
+        height = 0.5, -- Window height as a fraction of the editor height
+        row = 99, -- Row position for the window
       },
       mappings = {
         complete = {
-          detail = 'Use @<Tab> or /<Tab> for options.',
-          insert = '<Tab>',
+          detail = 'Use @<Tab> or /<Tab> for options.', -- Description for completion mapping
+          insert = '<Tab>', -- Key mapping for completion in insert mode
         },
         close = {
-          normal = 'q',
-          insert = '<C-c>',
+          normal = 'q', -- Key mapping to close in normal mode
+          insert = '<C-c>', -- Key mapping to close in insert mode
         },
         reset = {
-          normal = '<C-l>',
-          insert = '<C-l>',
+          normal = '<C-l>', -- Key mapping to reset in normal mode
+          insert = '<C-l>', -- Key mapping to reset in insert mode
         },
         submit_prompt = {
-          normal = '<CR>',
-          insert = '<C-s>',
+          normal = '<CR>', -- Key mapping to submit prompt in normal mode
+          insert = '<C-s>', -- Key mapping to submit prompt in insert mode
         },
         accept_diff = {
-          normal = '<C-y>',
-          insert = '<C-y>',
+          normal = '<C-y>', -- Key mapping to accept diff in normal mode
+          insert = '<C-y>', -- Key mapping to accept diff in insert mode
         },
         yank_diff = {
-          normal = 'gy',
-          register = '"',
+          normal = 'gy', -- Key mapping to yank diff in normal mode
+          register = '"', -- Register to use for yanking
         },
         show_diff = {
-          normal = 'gd',
+          normal = 'gd', -- Key mapping to show diff in normal mode
         },
         show_info = {
-          normal = 'gp',
+          normal = 'gp', -- Key mapping to show info in normal mode
         },
         show_context = {
-          normal = 'gs',
+          normal = 'gs', -- Key mapping to show context in normal mode
         },
       },
       -- See Configuration section for rest
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -23,15 +23,74 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
-
-    -- virtual-text
-    { ' theHamsta/nvim-dap-virtual-text', opts = {
-      virt_text_win_col = 80,
-    } },
+  },
+  keys = {
+    -- Basic debugging keymaps, feel free to change to your liking!
+    {
+      '<F5>',
+      function()
+        require('dap').continue()
+      end,
+      desc = 'Debug: Start/Continue',
+    },
+    {
+      '<F1>',
+      function()
+        require('dap').step_into()
+      end,
+      desc = 'Debug: Step Into',
+    },
+    {
+      '<F2>',
+      function()
+        require('dap').step_over()
+      end,
+      desc = 'Debug: Step Over',
+    },
+    {
+      '<F3>',
+      function()
+        require('dap').step_out()
+      end,
+      desc = 'Debug: Step Out',
+    },
+    {
+      '<leader>b',
+      function()
+        require('dap').toggle_breakpoint()
+      end,
+      desc = 'Debug: Toggle Breakpoint',
+    },
+    {
+      '<leader>B',
+      function()
+        require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+      end,
+      desc = 'Debug: Set Breakpoint',
+    },
+    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+    {
+      '<F7>',
+      function()
+        require('dapui').toggle()
+      end,
+      desc = 'Debug: See last session result.',
+    },
   },
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
+
+    -- Set up Go configuration
+    dap.configurations.go = {
+      {
+        type = 'go',
+        name = 'Debug API',
+        request = 'launch',
+        program = '${workspaceFolder}/cmd/api',
+        cwd = '${workspaceFolder}', -- assuming .env is at backend/.env
+      },
+    }
 
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
@@ -50,22 +109,12 @@ return {
       },
     }
 
-    -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<leader>dp', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<leader>do', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<leader>dO', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>dtb', function()
-      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-    end, { desc = 'Debug: Set Breakpoint' })
-
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
     dapui.setup {
       -- Set icons to characters that are more likely to work in every terminal.
       --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these s.
+      --    Don't feel like these are good choices.
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
       controls = {
         icons = {
@@ -82,8 +131,17 @@ return {
       },
     }
 
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    vim.keymap.set('n', '<leader>dtv', dapui.toggle, { desc = 'Debug: See last session result.' })
+    -- Change breakpoint icons
+    vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+    vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
+    local breakpoint_icons = vim.g.have_nerd_font
+        and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+      or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+    for type, icon in pairs(breakpoint_icons) do
+      local tp = 'Dap' .. type
+      local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+      vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+    end
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
