9c9aa15420ec95788bdd6c7f9895ca931b8dde06
iankendoit <iankencruz@gmail.com>
2025-05-07 13:18:37 +1000

    Add Go Formatters

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -33,7 +33,7 @@ return {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
-        --
+        go = { 'gofmt', 'goimports' },
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
